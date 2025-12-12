b8e8a21d564a791bcf7bc64342060c1dddf891d2
cww <cww@cwwdeMac-Studio.local>
2025-12-12 21:10:05 +0800

    feat: update

--- openopenwf/logger.cpp
+++ openopenwf/logger.cpp
@@ -6,9 +6,12 @@ void OpenWFLog(const std::string& message)
 {
 	auto lock = loggerLock.Acquire();
 
+	// 将UTF-8字符串转换为宽字符，以支持中文输出
+	std::wstring wideMessage = UTF8ToWide(message);
+
 	DWORD charsWritten;
-	WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), message.data(), (DWORD)message.size(), &charsWritten, nullptr);
-	WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), "\n", 1u, &charsWritten, nullptr);
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), wideMessage.data(), (DWORD)wideMessage.size(), &charsWritten, nullptr);
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), L"\n", 1u, &charsWritten, nullptr);
 }
 
 void OpenWFLogWide(const std::wstring& message)
@@ -24,12 +27,15 @@ void OpenWFLogColor(const std::string& message, unsigned short color)
 {
 	auto lock = loggerLock.Acquire();
 
+	// 将UTF-8字符串转换为宽字符，以支持中文输出
+	std::wstring wideMessage = UTF8ToWide(message);
+
 	DWORD charsWritten;
 	CONSOLE_SCREEN_BUFFER_INFO cbi;
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cbi);
 	WORD prevColor = cbi.wAttributes;
 
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-	WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), message.data(), (DWORD)message.size(), &charsWritten, nullptr);
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), wideMessage.data(), (DWORD)wideMessage.size(), &charsWritten, nullptr);
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), prevColor);
 }
